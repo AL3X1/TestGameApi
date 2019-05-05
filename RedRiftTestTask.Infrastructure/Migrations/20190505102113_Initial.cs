d416ff210b71b49f6a82bff68bec8b39b4a880af
alexinozemtsev <inzmtsv.alx@gmail.com>
2019-05-05 13:25:08 +0300

    Added fresh migrations.

---
+++ RedRiftTestTask.Infrastructure/Migrations/20190505102113_Initial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RedRiftTestTask.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    LobbyId = table.Column<Guid>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    WinnerId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lobbies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    HostName = table.Column<string>(nullable: true),
+                    PlayerCount = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lobbies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    LobbyId = table.Column<Guid>(nullable: false),
+                    Health = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Lobbies");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
